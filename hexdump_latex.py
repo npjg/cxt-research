1f46645c9a585c4cd09495cb353ca8baf6ac5633
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2021-03-05 17:51:18 -0500

    Upgrade to ansi2html parsing

--- hexdump_latex.py
+++ hexdump_latex.py
@@ -6,12 +6,17 @@ import re
 from strip_ansi import strip_ansi
 
 def parselines(lines, major_len=4, minor_len=4):
+    if args.with_ansi_colors:
+        from ansi2html import Ansi2HTMLConverter
+        conv = Ansi2HTMLConverter(latex=True, inline=True)
+    else:
+        conv = None
+
     latex = []
     fmt = "@{{}}c|*{{{}}}{{c@{{ }}}}c|c@{{}}".format(major_len*minor_len-1)
 
     for line in lines:
-        if not args.with_ansi_colors:
-            line = strip_ansi(line)
+        if not conv: line = strip_ansi(line)
 
         line = line.replace('\n', '')
         if line == '':
@@ -19,29 +24,25 @@ def parselines(lines, major_len=4, minor_len=4):
 
         theSplit = re.split('│', line, maxsplit=3)
 
-        theAddress = theSplit[0].strip()
-        theBytes = theSplit[1].strip().split()
-        theBytes +=  ["  "] * ((major_len*minor_len) - len(theBytes))
+        # Bug in the screen reset code of strip_ansi. It should filter away just an m.
+        theAddress = theSplit[0].strip()[3:]
+        theBytes = [conv.convert(byte, full=False) if conv else byte for byte in  theSplit[1].strip().split()][:-1]
+        theBytes +=  ["  "] * ((major_len*minor_len) - len(theBytes) - (0 if conv else 1))
         theAscii = theSplit[2].lstrip()
+        if conv: theAscii = conv.convert(theAscii, full=False)
+        if not args.with_unicode:
+            theAscii = re.sub(r'[^\x00-\x7F]','.', theAscii)
+        # theAscii = "".join(["\\verb│{}│".format(char) for char in theAscii])
 
-        latex.append("  {} \\\\".format(
-            " ﹠ ".join([theAddress, *theBytes, "\\texttt{{{}}}".format( # "\\verb│{}│".format(
-                theAscii if args.with_unicode else re.sub(r'[^\x00-\x7F]','.', theAscii)
-            )])))
+        latex.append("  {} \\\\".format(" & ".join([theAddress, *theBytes, "\\texttt{{{}}}".format(theAscii)]))) # "\\verb│{}│".format(
 
     if args.with_ansi_colors:
-        import subprocess
-        res = subprocess.run(["ansifilter", "-L"], input="\n".join(latex).encode('utf-8'), capture_output=True, check=True)
-        cells = res.stdout.decode("utf-8")
-        cells = cells.split("\n")
-        cells = "\n\n".join(cells[7:-3])
+        cells = conv.convert("\n".join(latex), full=False)
     else:
         cells = "\n".join(latex)
 
-    tabular = "\\UseRawInputEncoding\n\\begin{{tabular}}{{{}}}\n{}\n\\end{{tabular}}\n".format(fmt, cells.replace("﹠", "&"))
+    tabular = "\\UseRawInputEncoding\n\\begin{{tabular}}{{{}}}\n{}\n\\end{{tabular}}\n".format(fmt, cells)
     return tabular
-
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
