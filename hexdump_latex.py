e85b4414d983dff2859bd1d501fe1e4efc3e60b9
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2021-03-04 20:29:11 -0500

    Get columns and verbatims right in hexdump formatter

--- hexdump_latex.py
+++ hexdump_latex.py
@@ -1,24 +1,28 @@
+#/usr/bin/python
 
 import re
 from strip_ansi import strip_ansi
 
 def parselines(lines, major_len=4, minor_len=4):
     latex = []
-    fmt = "@{{}}c|*{{{}}}c|c@{{}}".format(major_len*minor_len)
-    latex.append(r"\begin{{tabular}}{{{}}}".format(fmt))
+    fmt = "@{{}}c|*{{{}}}{{c@{{ }}}}c|c@{{}}".format(major_len*minor_len-1)
+    latex.append("\\UseRawInputEncoding\n\\begin{{tabular}}{{{}}}".format(fmt))
 
     for line in lines:
         line = strip_ansi(line)
 
-        theSplit = [spec.strip() for spec in re.split(' │', line, maxsplit=3)]
-        # if len(theSplit) == 0:
-            # continue
+        theSplit = re.split(' │', line, maxsplit=3)
 
-        theBytes = theSplit[1].split()
-        theBytes +=  "" * ((major_len*minor_len) - len(theBytes))
+        theBytes = theSplit[1].strip().split()
+        theBytes +=  ["  "] * ((major_len*minor_len) - len(theBytes))
 
-        latex.append("  {} \\\\".format(" & ".join([theSplit[0], *theBytes, theSplit[2]])))
+        latex.append("  {} \\\\".format(
+            " & ".join([
+                theSplit[0][3:].strip(),
+                *theBytes,
+                "\\verbЯ{}Я".format(re.sub(r'[^\x00-\x7F]','.', theSplit[2])[1:-1])
+            ])))
 
-    latex.append(r"\end{tabular}")
+    latex.append("\\end{tabular}\n")
 
     return latex
--- hexdump_test.py
+++ hexdump_test.py
@@ -5,5 +5,8 @@ import pprint
 
 with open("dump.hex", 'r') as f:
     lines = f.readlines()
+    latex = hexdump_latex.parselines(lines[:-1])
+    # pprint.pprint(latex)
+
     with open("dump.tex", 'w') as g:
-        g.write("\n".join(hexdump_latex.parselines(lines[:-1])))
+        g.write("\n".join(latex))
