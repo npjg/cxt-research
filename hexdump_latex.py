c267fbea786b35b261336d4c4d51cc3defb52aae
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2021-03-04 20:51:35 -0500

    Add optional unicode inclusion

--- hexdump_latex.py
+++ hexdump_latex.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import argparse
 import re
 from strip_ansi import strip_ansi
 
@@ -16,19 +17,29 @@ def parselines(lines, major_len=4, minor_len=4):
 
         theSplit = re.split(' │', line, maxsplit=3)
 
+        theAddress = theSplit[0][3:].strip()
         theBytes = theSplit[1].strip().split()
         theBytes +=  ["  "] * ((major_len*minor_len) - len(theBytes))
+        theAscii = theSplit[2].lstrip()
 
         latex.append("  {} \\\\".format(
-            " & ".join([
-                theSplit[0][3:].strip(),
-                *theBytes,
-                "\\verb│{}│".format(re.sub(r'[^\x00-\x7F]','.', theSplit[2])[1:])
-            ])))
+            " & ".join([theAddress, *theBytes, "\\verb│{}│".format(
+                theAscii if args.with_unicode else re.sub(r'[^\x00-\x7F]','.', theAscii)
+            )])))
 
     latex.append("\\end{tabular}\n")
 
     return latex
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="hex2latex", formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    parser.add_argument(
+        "-U", "--with-unicode", action="store_true",
+        help="Include Unicode characters in TeX file (requires XeTeX to compile)"
+    )
+
+    args = parser.parse_args()
     print("\n".join(parselines(sys.stdin.readlines())))
